0abba1c00ce8fc43d788ba4c521169699d39ca2c
Sudipto Ghosh <75132686+Sudipto-Ghosh786@users.noreply.github.com>
2023-12-12 22:51:26 +0530

    Update Temp.sh

--- Temp.sh
+++ Temp.sh
@@ -1,2 +1,4 @@
 cd your_folder_path
 find . -name "*.java" -exec sed -i 's/class \([a-zA-Z0-9_]*\)/class Title_\1/g' {} \;
+cd your_folder_path
+find . -name "*.java" -exec sed -i 's/^import .*;/import xyz;/g' {} \;
